ea6cf5dfc4692954a1b2504e588bf842023587d4
Sean Norton <NortonSeanM@gmail.com>
2016-02-17 12:31:19 -0500

    add complete challenge w/o reflection

--- week-5/my_solution.rb
+++ week-5/my_solution.rb
@@ -1,6 +1,6 @@
 # Calculate the mode Pairing Challenge
 
-# I worked on this challenge [by myself, with: ]
+# I worked on this challenge [Kevin Fowle help form Jon Norstrom]
 
 # I spent [] hours on this challenge.
 
@@ -12,19 +12,37 @@
 
 # 0. Pseudocode
 
-# What is the input?
-# What is the output? (i.e. What should the code return?)
+# What is the input? list of numbers that we want to find the mode.
+# What is the output? (i.e. What should the code return?) The most used number or string(mode) in list/input
 # What are the steps needed to solve the problem?
+
+#Define mode method set argument
+#iterate the list
+# if value in list is == to another value
+#put value into array.
+
 
 
 # 1. Initial Solution
+def mode(array)
+  counter = Hash.new(0)
+  array.each do |i|
+    counter[i] += 1
+  end
 
+  mode_array = []
+  counter.each do |k, v|
+    if v == counter.values.max
+      mode_array << k
+    end
 
+  end
+  mode_array.sort
+end
 
 
 # 3. Refactored Solution
-
-
+  #same as above#
 
 
 # 4. Reflection
