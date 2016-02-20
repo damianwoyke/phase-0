db1b4542ddc136f74546c49de31b7c4ebc50fef3
Sean Norton <NortonSeanM@gmail.com>
2016-02-20 17:21:53 -0600

    add reflection

--- week-5/nums-commas/my_solution.rb
+++ week-5/nums-commas/my_solution.rb
@@ -1,6 +1,6 @@
 # Numbers to Commas Solo Challenge
 
-# I spent [] hours on this challenge.
+# I spent [1] hours on this challenge.
 
 # Complete each step below according to the challenge directions and
 # include it in this file. Also make sure everything that isn't code
@@ -18,7 +18,7 @@
 #return string with comma
 
 
-# 1. Initial Solution
+1. Initial Solution
 def separate_comma(num)
   num_string = num.to_s
 
@@ -36,8 +36,26 @@ def separate_comma(num)
   end
 
 # 2. Refactored Solution
+## Take argument number change it to a string, then take that strings characters and set it to an array, this way you can iterate using .each_slice. reverse the array so the commas fall in the correct place from left to right. slice it into threes, map then joins the slices of 3, then join the slices with a "," and reverse it back.
 
+def separate_comma(num)
+  num.to_s.chars.to_a.reverse.each_slice(3).map(&:join).join(",").reverse
+end
 
 
 
 # 3. Reflection
+# What was your process for breaking the problem down? What different approaches did you consider?
+  #I had to think about a few different ways that I could solve the same problem. My first go to was to think of how and when numbers use commas and find any pattern. Then how to get the patter to work. At first I thought of a similar way that my refactored solution ended up being, but I couldn't figure out how to write that without using ruby docs. So I had to re-think and it turned out being more simple to understand solution in my opinion.
+
+# Was your pseudocode effective in helping you build a successful initial solution?
+ #pseudocode was my saviour, I think I spent a moajority of my time working on the pseudocode. And after doing that for this challenge, I'd like to spend more time on it in future challenges.
+
+# What new Ruby method(s) did you use when refactoring your solution? Describe your experience of using the Ruby documentation to implement it/them (any difficulties, etc.). Did it/they significantly change the way your code works? If so, how?
+#My entire refactored solution was pretty much filled with new ruby methods. I have a pretty good decription as to what each things is doing right above my code. I had most of this down, I just didn't know that I needed .map or how it worked orignially, but after lookin up the solution I found .map and read up on it a little.
+
+# How did you initially iterate through the data structure?
+  # I iterated through the lenght of the number and added a comma every three places by assigning a variable to the length and then subtracting three, as long as variable was > 3 it would keep going, this insured that no commas would be added to the begining of a number like ,100,000.
+
+# Do you feel your refactored solution is more readable than your initial solution? Why?
+  # I think that if you know that the ruby methods do then yes it is eaiser to read, it's all in one line and you don't have to look out and around for where variables were assigned and things of that nature. Though I think for a beginer that my initial solution would be eaiser to figure out what is going on.
