cbd07b491881a6a07cd6137bde5adac44e6fb3dd
Sean Norton <NortonSeanM@gmail.com>
2016-02-23 18:08:50 -0600

    complete half of challenge.

--- week-6/credit-card/my_solution.rb
+++ week-6/credit-card/my_solution.rb
@@ -1,31 +1,57 @@
 # Class Warfare, Validate a Credit Card Number
 
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge [with: ].
 # I spent [#] hours on this challenge.
 
 # Pseudocode
 
-# Input:
-# Output:
-# Steps:
-
+# Input: 16 digit number
+# Output: check if number is true
+# Step 1: take in original number
+# Step 2: double every other number from last  - first
+# Step 3: Break double digits apart and add the integers together
+# Step 4:
+# Step 5: Profit
 
 # Initial Solution
+
+
 
 # Don't forget to check on initialization for a card length
 # of exactly 16 digits
 
 class CreditCard
 
+    def initialize(number)
+        @number = number
+
+         raise ArgumentError.new("Needs number") if number.to_s.length != 16
+    end
+
+    def check_card
+    @new_array = @number.to_s.split(//)
+    @new_array.map!.with_index do |num, index|
+     if index % 2 == 0
+       num.to_i * 2
+     else
+        num.to_i
+    end
+  end
+    end
+
+
 end
 
+card = CreditCard.new(4408041234567901)
+p card.check_card
 
 
 # Refactored Solution
-
-
-
+  #def check_card
+    # valid = new.Array
+    # valid << @number.split(//)
+#end
 
 
 
