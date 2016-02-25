046a89f40dfd06bafda0570995be42400dd04b70
Sean Norton <NortonSeanM@gmail.com>
2016-02-25 15:36:19 -0600

    add reflection

--- week-6/credit-card/my_solution.rb
+++ week-6/credit-card/my_solution.rb
@@ -1,8 +1,7 @@
 # Class Warfare, Validate a Credit Card Number
 
-
-# I worked on this challenge [with: ].
-# I spent [#] hours on this challenge.
+# I worked on this challenge [with: Tom Sok ].
+# I spent [3] hours on this challenge.
 
 # Pseudocode
 
@@ -16,68 +15,67 @@
 
 # Initial Solution
 
-
-
 # Don't forget to check on initialization for a card length
 # of exactly 16 digits
 
 class CreditCard
-
   def initialize(number)
-      @number = number
-      raise ArgumentError.new("Needs number") if number.to_s.length != 16
+    @number = number
+    raise ArgumentError.new('Needs number') if number.to_s.length != 16
   end
+
   def check_card
     @new_array = @number.to_s.split(//)
     @new_array.map!.with_index do |num, index|
-      if index % 2 == 0
+      if index.even?
         num.to_i * 2
       else
         num.to_i
       end
-      end
-      @new_array = @new_array.join('').split(//)
-      @new_array.map! do |num|
-         num.to_i
-       end
-      if @new_array.inject {|sum,i| sum+i} % 10 == 0
-         true
-      else
-        false
-      end
+    end
+    @new_array = @new_array.join('').split(//)
+    @new_array.map!(&:to_i) #@new_array.map!{|x| x.to_i}
+    if @new_array.inject { |sum, i| sum + i } % 10 == 0
+      true
+    else
+      false
+    end
   end
 end
 
-card = CreditCard.new(4408041234567901)
+card = CreditCard.new(4_408_041_234_567_901)
 p card.check_card
-
 
 # Refactored Solution
 
 class CreditCard
   def initialize(number)
-      @number = number
-      raise ArgumentError.new("Needs number") if number.to_s.length != 16
+    @number = number
+    raise ArgumentError.new('Needs number') if number.to_s.length != 16
   end
 
   def check_card
     @number = @number.to_s.split(//)
     @number.map!.with_index do |num, index|
-      index % 2 == 0 ? num.to_i * 2 : num.to_i
+      index.even? ? num.to_i * 2 : num.to_i
     end
 
     @number = @number.join('').split(//)
-    @number.map!{|num| num.to_i}
-    @number.inject {|sum,i| sum+i} % 10 == 0 ? true : false
+    @number.map!(&:to_i)
+    @number.inject { |sum, i| sum + i } % 10 == 0 ? true : false
   end
 end
 
 card = CreditCard.new(4408041234567901)
 p card.check_card
 
-
-
-
-
-
 # Reflection
+
+# What was the most difficult part of this challenge for you and your pair?
+  # I think we were origanly overthinking things, It took us an hour and a half just to get the first section of check_card done. I think we were overthinking things, and our argument error wasn't working right when we ran ruby but did in rspec, so we were having a hard time with testing.
+
+# What new methods did you find to help you when you refactored?
+  # we found ? to come in handy, this helped shorten the if statments to one line. as well as &: to shorten {|x| x.method}
+
+# What concepts or learnings were you able to solidify in this challenge?
+  # I was able to solidify iterating through arrays and methods such as .split and join.
