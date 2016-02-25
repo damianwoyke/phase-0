309a62f3ffa4cfe7a9557aa7c977c2b0bcdf71d1
Sean Norton <NortonSeanM@gmail.com>
2016-02-25 16:49:37 -0600

    add reflection to die-class2

--- week-6/die-2/my_solution.rb
+++ week-6/die-2/my_solution.rb
@@ -6,10 +6,11 @@
 
 # Pseudocode
 
-# Input:
-# Output:
-# Steps:
-
+# Input: string for vlaues on die
+# Output: number of sides based on number of strings in array & random string in array.
+# Steps: raise error if sides(labels.length) is less than 1
+#define sides
+#sample from array when rolling
 
 # Initial Solution
 
@@ -35,7 +36,18 @@ end
 
 
 # Refactored Solution
-
+class Die
+  def initialize(labels)
+    @labels = labels
+    if labels.length > 0
+        @sides = labels.length
+      else
+        raise ArgumentError, "Needs more sides!"
+    end
+  end
+  def sides; @sides ; end
+  def roll; @labels.sample ; end
+end
 
 
 
@@ -44,3 +56,14 @@ end
 
 
 # Reflection
+
+# What were the main differences between this die class and the last one you created in terms of implementation? Did you need to change much logic to get this to work?
+# It was almost the exact same, instead of rand I had to use sample, since it was choosing from an array.
+
+# What does this exercise teach you about making code that is easily changeable or modifiable?
+# well written easy to read code can be changed eaisly for different similar uses.
+# What new methods did you learn when working on this challenge, if any?
+# no new methods.
+
+# What concepts about classes were you able to solidify in this challenge?
+# instance variables
